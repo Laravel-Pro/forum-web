dcd04739ccf76556e33619f9ce6c08643e5e55da
Wkan <wangkan1119@gmail.com>
2020-02-26 15:12:21 +0800

    fix: 修改测试标题

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('have Laravel Pro title', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Laravel Pro/i);
   expect(linkElement).toBeInTheDocument();
