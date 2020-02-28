c8becf4cf0abe667256a02bd3e39494f3509842a
Wkan <wangkan1119@gmail.com>
2020-02-28 13:20:36 +0800

    使用js配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,34 @@
-{
-  "extends": [
-    "react-app",
-    "airbnb-base",
-    "plugin:cypress/recommended"
+const path = require('path');
+
+module.exports = {
+  extends: [
+    'react-app',
+    'airbnb-base',
+    'plugin:cypress/recommended'
   ],
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "*.test.js",
-        "*.spec.js"
+      files: [
+        '*.test.js',
+        '*.spec.js'
       ],
-      "rules": {
-        "no-unused-expressions": "off",
+      rules: {
+        'no-unused-expressions': 'off',
+      }
+    },
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname, 'src')],
       }
     }
-  ],
-  "rules": {
-    "import/no-unresolved": [
-      "error",
-      {
-        "ignore": [ "src/" ]
-      }
-    ]
   },
-  "globals": {
-    "provider": true,
-    "jasmine": true
+  globals: {
+    provider: true,
+    jasmine: true
   }
-}
+};
