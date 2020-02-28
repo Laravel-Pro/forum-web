c603617efbc9319806df1fadec445e276f5d6977
Wkan <wangkan1119@gmail.com>
2020-02-28 12:48:49 +0800

    添加 pact 测试

---
+++ src/services/pact-provider.js
@@ -0,0 +1,13 @@
+import { Pact } from '@pact-foundation/pact';
+import path from "path";
+
+export const provider = new Pact({
+  consumer: 'forum-web',
+  provider: 'forum-server',
+  pactfileWriteMode: 'update',
+  log: path.resolve(process.cwd(), 'pact.log'),
+  logLevel: 'warn',
+  dir: path.resolve(process.cwd(), 'pacts'),
+  cors: true,
+  spec: 2,
+});
