7692873df26ad3b8b8092d3a0663e15b750388d7
Wkan <wangkan1119@gmail.com>
2020-02-28 12:46:39 +0800

    添加request方法

---
+++ src/utils/request.js
@@ -0,0 +1,42 @@
+let baseURL = window ? window.location.href : '';
+
+// for pact test
+export function setBaseURL(url) {
+  baseURL = url;
+}
+
+export default function request(url, options) {
+  const defaultOptions = {};
+  const newOptions = {
+    ...defaultOptions,
+    ...options,
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  if (newOptions.method === 'POST' || newOptions.method === 'PUT' || newOptions.method === 'DELETE') {
+    if (!(newOptions.body instanceof FormData)) {
+      newOptions.headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        ...newOptions.headers,
+      };
+      newOptions.body = JSON.stringify(newOptions.body);
+    } else {
+      // newOptions.body is FormData
+      newOptions.headers = {
+        'Content-Type': 'multipart/form-data',
+        ...newOptions.headers,
+      };
+    }
+  }
+
+  const resolvedUrl = (new URL(url, baseURL)).href;
+
+  return fetch(resolvedUrl, newOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+}
---
+++ src/utils/request.test.js
@@ -0,0 +1,24 @@
+import request from 'utils/request';
+
+describe('request', () => {
+  it('should parse json response body', async () => {
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve(new Response('{"foo":"bar"}')));
+
+    const data = await request('/');
+
+    expect(data).toHaveProperty('foo', 'bar');
+  });
+
+  it('should stringify as json when request body is not FormData', async () => {
+    const body = {foo: 'bar'};
+    global.fetch = jest.fn().mockImplementation(async (url, options) => {
+      expect(options.body).toBe(JSON.stringify(body));
+      return new Response(JSON.stringify(body));
+    });
+
+    await request('/', {
+      method: 'POST',
+      body: body,
+    });
+  });
+})
