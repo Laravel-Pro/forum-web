9da6208e594d0b487086a6b181ddc0ef30dcf97e
Wkan <wangkan1119@gmail.com>
2020-02-28 16:19:33 +0800

    chore: 使用package.json 里的版本号

--- scripts/publish-pact.js
+++ scripts/publish-pact.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const publisher = require('@pact-foundation/pact-node')
-const path = require('path')
+const publisher = require('@pact-foundation/pact-node');
+const path = require('path');
+const version = require('../package.json').version;
 
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 
 require('../config/env');
 
-let opts = {
+const opts = {
   pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')],
   pactBroker: process.env.PACT_BROKER_URL,
   pactBrokerUsername: process.env.PACT_USERNAME,
   pactBrokerPassword: process.env.PACT_PASSWORD,
-  consumerVersion: '2.0.0',
+  consumerVersion: version,
 }
 
 publisher.publishPacts(opts)
