156d3acf6649ab546a864360976497c553de1581
Wkan <wangkan1119@gmail.com>
2020-03-08 22:06:37 +0800

    移除App测试

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('have Laravel Pro title', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Laravel Pro/i);
-  expect(linkElement).toBeInTheDocument();
-});
