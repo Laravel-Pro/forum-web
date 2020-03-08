111b355890eca97177ff8bc9b723d0a75b79c395
Wkan <wangkan1119@gmail.com>
2020-03-08 23:35:21 +0800

    添加ProfileToggle组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import ChannelTab from 'components/ChannelTab/ChannelTab';
 import Header from 'components/Header/Header';
+import ProfileToggle from 'components/ProfileToggle/ProfileToggle';
 
 export {
   ChannelTab,
   Header,
+  ProfileToggle,
 };
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+
+jest.mock('popper.js', () => {
+  const PopperJS = jest.requireActual('popper.js');
+
+  return class {
+    static placements = PopperJS.placements;
+
+    constructor() {
+      return {
+        destroy: () => {},
+        scheduleUpdate: () => {},
+      };
+    }
+  };
+});
