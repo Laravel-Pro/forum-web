099cd1c6f0faf9beb2a1e5685c783428d45047a8
Wkan <wangkan1119@gmail.com>
2020-03-08 23:12:49 +0800

    feat: header 添加额外信息位

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-function Header() {
+function Header({ extra }) {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Laravel Pro</Navbar.Brand>
+      <Navbar.Brand className="mr-auto" href="/">Laravel Pro</Navbar.Brand>
+      {extra}
     </Navbar>
   );
 }
+
+Header.propTypes = {
+  extra: PropTypes.node,
+};
+
+Header.defaultProps = {
+  extra: '',
+};
 
 export default Header;
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -7,3 +7,10 @@ it('should show Laravel Pro', () => {
   const allTab = getByText(/Laravel Pro/);
   expect(allTab).toBeInTheDocument();
 });
+
+it('should have extra content', () => {
+  const extraContent = 'here is extra';
+  const { getByText } = render(<Header extra={extraContent} />);
+  const extra = getByText(RegExp(extraContent));
+  expect(extra).toBeInTheDocument();
+});
