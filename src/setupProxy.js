373c96746f7e49bff4f2783e82373f2c95b710ed
Wkan <wangkan1119@gmail.com>
2020-03-13 16:14:10 +0800

    chore: 添加 avatar 目录代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use(
-    '/api',
+    '/api|/avatar',
     createProxyMiddleware({
       target: process.env.API_PROXY || 'http://localhost:8000',
       changeOrigin: true,
