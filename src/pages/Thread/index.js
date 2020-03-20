6ddc95fb9bd84bd212769b7a036ffcd800ee785b
Wkan <wangkan1119@gmail.com>
2020-03-20 15:04:18 +0800

    chore: 移除多余的 Container

--- src/pages/Thread/index.js
+++ src/pages/Thread/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
-import { Badge, Container, Media } from 'react-bootstrap';
+import { Badge, Media } from 'react-bootstrap';
 import showdown from 'showdown';
 import UserContext from 'UserContext';
 import { getThread } from 'services/thread';
@@ -59,40 +59,38 @@ function Thread() {
   } = thread;
 
   return (
-    <Container className="mt-2">
-      <div className="thread-detail">
-        <Media className="thread p-2">
-          <Media.Body>
-            <h5 className="thread-title">
-              <Badge variant="light" className="mr-2 channel-name">{channel.name}</Badge>
-              {title}
-            </h5>
-            <span className="text-muted small">{`由 ${author.username} 发布于 ${createdAt}`}</span>
-          </Media.Body>
-          <img
-            width={64}
-            height={64}
-            className="align-self-end ml-auto"
-            src={author.avatar_url}
-            alt={`avatar of ${author.username}`}
-          />
-        </Media>
-        {/* eslint-disable-next-line react/no-danger */}
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        <hr />
-        <ReplyList
-          replies={replies}
-          onReply={handleReply}
-          pagination={{
-            ...pagination,
-            onChange: (page) => {
-              setPagination({ ...pagination, currentPage: page });
-            },
-          }}
-          canReply={!!user.id}
+    <div className="thread-detail">
+      <Media className="thread p-2">
+        <Media.Body>
+          <h5 className="thread-title">
+            <Badge variant="light" className="mr-2 channel-name">{channel.name}</Badge>
+            {title}
+          </h5>
+          <span className="text-muted small">{`由 ${author.username} 发布于 ${createdAt}`}</span>
+        </Media.Body>
+        <img
+          width={64}
+          height={64}
+          className="align-self-end ml-auto"
+          src={author.avatar_url}
+          alt={`avatar of ${author.username}`}
         />
-      </div>
-    </Container>
+      </Media>
+      {/* eslint-disable-next-line react/no-danger */}
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <hr />
+      <ReplyList
+        replies={replies}
+        onReply={handleReply}
+        pagination={{
+          ...pagination,
+          onChange: (page) => {
+            setPagination({ ...pagination, currentPage: page });
+          },
+        }}
+        canReply={!!user.id}
+      />
+    </div>
   );
 }
 
